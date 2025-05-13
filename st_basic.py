29fdb37006a40ee9f046050ae5795e3ba30b35e3
Chunghun Ha <chunghun.ha@gmail.com>
2025-05-13 17:44:35 +0900

    Update st_basic.py

--- st_basic.py
+++ st_basic.py
@@ -2,5 +2,5 @@ import streamlit as st  # streamlit 라이브러리 임포트
 
 # 타이틀 텍스트 출력
 st.title('첫번째 웹 어플 만들기 👋📺🙋🏻‍♂️')
-st.title('fghjfghjf첫번째 웹 어플 만들기 👋📺🙋🏻‍♂️')
-st.title('fghjfghjf첫번째 웹 어플 만들기 👋📺🙋🏻‍♂️')
+st.title('두번째 웹 어플 만들기 👋📺🙋🏻‍♂️')
+st.title('세번째 웹 어플 만들기 👋📺🙋🏻‍♂️')
